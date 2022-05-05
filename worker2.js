1dbe22bd3074b3c418c2d78fa1259db52621e4c7
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-05-05 16:07:26 +0700

    feat(worker2): better isPrime function
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- worker2.js
+++ worker2.js
@@ -1,7 +1,14 @@
 module.exports = ({ a }) => {
-  if (a % 2 === 0) {
-    return "prime";
-  } else {
+  if (a < 2) {
     throw new Error("bukan prime");
   }
+
+  for (let i = 2; i <= Math.sqrt(a); i++) 
+  {
+    if (a % i === 0) {
+      throw new Error("bukan prime");
+    }
+  }
+
+  return "prime";
 }
